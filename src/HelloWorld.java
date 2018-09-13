e931cfbd7c199d6b4030099f32428c5ecf9bde35
ctroina <ctroina@student.mamkschools.org>
2018-09-13 10:05:17 -0400

    Initial2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world!\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("yes");
     }
 }
