4b770e2981ae6e59d434d966f1b1b3c05481c9b5
ctroina <ctroina@student.mamkschools.org>
2018-09-18 09:56:57 -0400

    ArrayPractice2

--- src/CH7/ArrayPractice.java
+++ src/CH7/ArrayPractice.java
@@ -1,4 +1,27 @@
 package CH7;
 
+import java.util.Arrays;
+
 public class ArrayPractice {
+    public static void main(String []args){
+        /*int[] numbers=new int[10];
+        numbers[0]=1;
+        for(int i=1;i<=9;i++){
+            numbers[i]=i+1;
+        }
+        for(int i=0;i<=9;i++){
+            System.out.println(numbers[i]);
+        }*/
+        int[] numbers = new int[5];
+        //An integer array of size 5
+        System.out.println(Arrays.toString(numbers));
+        //before populating the array
+        for(int i=0;i<=4;i++){
+            numbers[i]=i+1;
+        }
+        System.out.println(Arrays.toString(numbers));
+        //after populating the array
+        int[] numbers2={1,2,3,4,5};
+        System.out.println(Arrays.toString(numbers2));
+    }
 }
---
+++ src/CH7/WeatherAnalysis.java
@@ -0,0 +1,20 @@
+package CH7;
+
+import java.util.Scanner;
+
+/*This program takes user input and calculates the average temperature for n days. The program also identifies how many days are
+above average*/
+public class WeatherAnalysis {
+    public static void main(String[] args){
+        //Step 1: Take user input for number of days and temperatures
+        //Step 2: Record number of days with an array and a cumulative sum variable
+        //Step 3: Calculate the average
+        //Step 4: Figure out which days are above average
+        //Step 5: Print results
+        Scanner console= new Scanner(System.out);
+        weather(console);
+    }
+    public static void weather(Scanner console){
+
+    }
+}
