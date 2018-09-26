98e9d7459d46fc628ddd42e6c9a58303beea489f
ctroina <ctroina@student.mamkschools.org>
2018-09-26 09:27:24 -0400

    Weather Analysis 2 Final

--- src/CH7/ChristopherTroinaWeatherAnalysis2.java
+++ src/CH7/ChristopherTroinaWeatherAnalysis2.java
@@ -19,6 +19,11 @@ public class ChristopherTroinaWeatherAnalysis2 {
         System.out.print("How many days' temperatures? ");
         //User input for number of days
         int days=console.nextInt();
+        //Catches number lower than 2
+        while(days<=1){
+            System.out.print("Bad number, put in a value higher than 1. ");
+            days=console.nextInt();
+        }
         int[] temps=new int[days];
         //array for storing user input for temperature.
         int sum=0;
