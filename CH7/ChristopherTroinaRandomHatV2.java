e4103a026d0d3b22ead904f6e30ea7f837a1d3c1
ctroina <ctroina@student.mamkschools.org>
2018-10-04 10:24:14 -0400

    New location

---
+++ CH7/ChristopherTroinaRandomHatV2.java
@@ -0,0 +1,13 @@
+package CH7;
+
+import java.util.Scanner;
+
+public class ChristopherTroinaRandomHatV2 {
+    public static void main(String[] Args){
+        numGen();
+    }
+    public static void numGen(){
+        Scanner console=new Scanner(System.in);
+        int[] num
+    }
+}
