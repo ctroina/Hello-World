2e72475adade83f5d0ec143dc7349b47dfcc2085
ctroina <ctroina@student.mamkschools.org>
2018-10-04 09:51:13 -0400

    New location

--- src/src/CH7/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package CH7;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello world!\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("yes");
-    }
-}
