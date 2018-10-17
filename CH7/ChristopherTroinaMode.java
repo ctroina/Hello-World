90b11c380947177586fa8a47fec3379edc092724
ctroina <ctroina@student.mamkschools.org>
2018-10-17 09:30:24 -0400

    Mode final ver

--- CH7/ChristopherTroinaMode.java
+++ CH7/ChristopherTroinaMode.java
@@ -1,16 +1,21 @@
 package CH7;
-
+//This program finds the most frequent digit in a number and return the most frequent digit
 public class ChristopherTroinaMode {
     public static void main(String[] args){
-        System.out.println("Most Frequent: "+ modeV2(669260267));
+        //prints result
+        System.out.println("Most Frequent: "+ mode(669260267));
     }
-    public static int modeV2(int n){
+    public static int mode(int n){
+        //counter variable for number frequency
         int[] count=new int[10];
         while(n>0){
+            //fills counter variable
             count[n%10]++;
             n/=10;
         }
+        //variable to store maximum value of counter variable
         int max=count[0];
+        //variable to store index of maximum value of counter variable.
         int maxin=0;
         for(int i=1;i<count.length;i++){
             if(Math.max(max, count[i])==count[i]&&max!=count[i]){
@@ -18,6 +23,7 @@ public class ChristopherTroinaMode {
                 maxin=i;
             }
         }
+        //returns most frequent digit
         return maxin;
     }
 }
