41db034b0ac50362fc7a25fc75626870f0b65b8e
ctroina <ctroina@student.mamkschools.org>
2018-11-02 09:56:21 -0400

    2d arrays

---
+++ CH7/ChristopherTroinaSum.java
@@ -0,0 +1,11 @@
+package CH7;
+
+public class ChristopherTroinaSum {
+    public static void main(String[] args){
+        //Step 1: Create a scanner to read input file
+        //Step 2: Store first line from input file in a string
+        //Step 3: Create a variable to store the number of inputs
+        //Step 4: Create an array with a length of 125
+        //Step 5:
+    }
+}
