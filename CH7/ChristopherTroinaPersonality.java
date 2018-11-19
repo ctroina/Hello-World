fff35b5122246e9cfa940e879e248034a8b77cf9
ctroina <ctroina@student.mamkschools.org>
2018-11-19 09:56:15 -0500

    Personality test

--- CH7/ChristopherTroinaPersonality.java
+++ CH7/ChristopherTroinaPersonality.java
@@ -1,6 +1,7 @@
 package CH7;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
@@ -15,8 +16,42 @@ public class ChristopherTroinaPersonality {
             filename=console.next();
             file=new File(filename);
         }
-        System.out.print("Output file name: ");
-        String outfile=console.next();
-        PrintStream stream=new PrintStream(outfile);
+        try {
+            Scanner input = new Scanner(file);
+            System.out.print("Output file name: ");
+            String outfile=console.next();
+            try {
+                PrintStream stream = new PrintStream(outfile);
+                fileScan(input, stream);
+            }
+            catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    public static void fileScan(Scanner input, PrintStream stream){
+        int count=1;
+        while(input.hasNextLine()){
+            if(count/2*2!=count){
+                String line=input.nextLine();
+                Scanner linescan= new Scanner(line);
+                while(linescan.hasNext())
+                    stream.print(linescan.next());
+                stream.println();
+            }
+            else{
+                count=0;
+                int[][] asbs=new int[4][2];
+                String answers=input.next();
+                for(int i=1;i<=10;i++){
+                    for(int ii=7;ii<=10;ii++){
+                        
+                    }
+                }
+            }
+        }
     }
 }
