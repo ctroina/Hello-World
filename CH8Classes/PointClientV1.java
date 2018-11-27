df83aea81f27c952550071fd854e2dcaf0872815
ctroina <ctroina@student.mamkschools.org>
2018-11-27 09:56:15 -0500

    Personality test

---
+++ CH8Classes/PointClientV1.java
@@ -0,0 +1,11 @@
+package CH8Classes;
+
+public class PointClientV1 {
+    public static void main(String[] args){
+        PointV1 p1=new PointV1();
+        System.out.println("p1: ("+p1.x+", "+p1.y+")");
+        p1.x=3;
+        p1.y=6;
+        System.out.println("p1': ("+p1.x+", "+p1.y+")");
+    }
+}
