231277ea915f729996c04704543ca9d431552d38
ctroina <ctroina@student.mamkschools.org>
2018-11-27 10:00:15 -0500

    Point

---
+++ CH8Classes/PointV1.java
@@ -0,0 +1,13 @@
+package CH8Classes;
+//This class is a template to represent a point on a coordinate plane
+public class PointV1 {
+    public int x;
+    public int y;
+    //behaviors below (methods)
+    public void translate(int x, int y){
+
+    }
+    public void setLocation(int x, int y){
+
+    }
+}
