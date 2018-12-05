56bc7304fbe24338f7cf8bb163d9c06721b6271e
ctroina <ctroina@student.mamkschools.org>
2018-12-05 09:42:37 -0500

    Point

---
+++ CH8Classes/PointClientV4.java
@@ -0,0 +1,14 @@
+package CH8Classes;
+
+public class PointClientV4 {
+    public static void main(String[] args){
+        PointV4 p1=new PointV4(3, 6);
+        System.out.println("p1: "+p1);
+        PointV4 p2=new PointV4(7, 25);
+        System.out.println("p2: "+p2);
+        PointV4 p3=new PointV4();
+        System.out.println("p3: "+p3);
+        System.out.println(p1.distance(p2));
+        System.out.println(p1.distanceFromOrigin());
+    }
+}
