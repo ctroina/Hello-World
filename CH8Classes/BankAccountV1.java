70b44d7d93f4ca1c23303936aec85fe9bb51d69d
ctroina <ctroina@student.mamkschools.org>
2018-12-10 09:56:02 -0500

    Bank account

---
+++ CH8Classes/BankAccountV1.java
@@ -0,0 +1,10 @@
+package CH8Classes;
+//this program is a very real bank account
+public class BankAccountV1 {
+    private int pin;
+    private String name;
+    private String ssn;
+    private String Adress;
+    private int[] dob;
+    
+}
