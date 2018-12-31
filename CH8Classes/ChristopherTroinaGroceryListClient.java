6e2c5abe711e1abbdb1efd8086e394f0c240d361
ctroi <ctroina@student.mamkschools.org>
2018-12-31 15:07:54 -0500

    list

--- CH8Classes/ChristopherTroinaGroceryListClient.java
+++ CH8Classes/ChristopherTroinaGroceryListClient.java
@@ -1,7 +1,16 @@
 package CH8Classes;
 
+/**
+ * ChristopherTroinaGroceryListClient, Runs the the ChristopherTroinaGroceryList class.
+ * @author Christioher Troina
+ */
 public class ChristopherTroinaGroceryListClient {
+    /**
+     * Creates a new ChristopherTroinaGroceryList object, adds ten items to it, and prints it.
+     */
     public static void main(String[] args){
+        /**The ChristopherTroinaGroceryList object used to test the ChristopherTroinaGroceryList and
+         *ChristopherTroinaGroceryItemOrder classes*/
         ChristopherTroinaGroceryList list=new ChristopherTroinaGroceryList();
         list.addItem(new ChristopherTroinaGroceryItemOrder("Napkins", 2, 5));
         list.addItem(new ChristopherTroinaGroceryItemOrder("Ham", 10, 15));
@@ -13,7 +22,6 @@ public class ChristopherTroinaGroceryListClient {
         list.addItem(new ChristopherTroinaGroceryItemOrder("Milk", 4, 13.21));
         list.addItem(new ChristopherTroinaGroceryItemOrder("Egg", 1, .05));
         list.addItem(new ChristopherTroinaGroceryItemOrder("$375", 10, 0));
-        list.addItem(new ChristopherTroinaGroceryItemOrder("Bread", 999, 1.99));
-        System.out.println(list.getTotalCost());
+        System.out.print(list);
     }
 }
